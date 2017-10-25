e7c3fb2a834d976188c67887c62f8d51e4a5f4e8
Kelseys1993 <31227275+Kelseys1993@users.noreply.github.com>
2017-10-25 21:05:15 +1300

    added sounds

--- script.js
+++ script.js
@@ -7,8 +7,16 @@ init();
 			window.newAudioB = new Audio();
 			newAudioB.src = 'sounds/guitar.wav';
 
-			// window.newAudioC = new Audio();
-			// newAudioC.src = 'c.wav';
+			window.newAudioC = new Audio();
+			newAudioC.src = 'sounds/saxaphone.wav';
+
+			window.newAudioD = new Audio();
+			newAudioD.src = 'sounds/trumpet.mp3';
+
+
+			window.newAudioE = new Audio();
+			newAudioE.src = 'sounds/cymbal.mp3';
+
 		}();
 	
 	
@@ -19,9 +27,17 @@ init();
 		var playButton = document.getElementById('guitar');
 		playButton.addEventListener('click', playAudioB, false);
 
-		// var playButton = document.getElementById('box3');
-		// playButton.addEventListener('click', playAudioC, false);
+		var playButton = document.getElementById('sax');
+		playButton.addEventListener('click', playAudioC, false);
+
+		var playButton = document.getElementById('trumpet');
+		playButton.addEventListener('click', playAudioD, false);
+
+
+		var playButton = document.getElementById('cymbal');
+		playButton.addEventListener('click', playAudioE, false);
 	}
+
 
 	
 	function playAudioA() {
@@ -30,10 +46,15 @@ init();
 	function playAudioB() {
 		newAudioB.play()
 	}
-	// function playAudioC() {
-	// 	newAudioC.play()
-	// }
+	function playAudioC() {
+		newAudioC.play()
+	}
 
+	function playAudioD() {
+		newAudioD.play()
+	}
 
-	
+	function playAudioE() {
+		newAudioE.play()
+	}
 
