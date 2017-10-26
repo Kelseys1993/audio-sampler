c5af3df40a6f9010f37dd511022a77b692747a05
Kelsey Smith <845872897@ken20-202-21.local>
2017-10-26 13:51:50 +1300

    finished script

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 
-
+	
+	(function(){
 
 init();
 
@@ -20,6 +21,20 @@ init();
 			window.newAudioE = new Audio();
 			newAudioE.src = 'sounds/cymbal.mp3';
 
+			window.newAudioF = new Audio();
+			newAudioF.src = 'sounds/piano.wav';
+
+			// Lewis
+			window.newAudioG = new Audio();
+			newAudioG.src = 'sounds/harmonica.wav';
+			// Lewis
+			window.newAudioH = new Audio();
+			newAudioH.src = 'sounds/bass.mp3';
+			// Lewis
+			window.newAudioI = new Audio();
+			newAudioI.src = 'sounds/violin.wav';
+
+
 		}();
 	
 	
@@ -39,25 +54,95 @@ init();
 
 		var playButton = document.getElementById('cymbal');
 		playButton.addEventListener('click', playAudioE, false);
+
+		var playButton = document.getElementById('piano');
+		playButton.addEventListener('click', playAudioF, false);
+
+		// Lewis
+		var playButton = document.getElementById('harmonica');
+		playButton.addEventListener('click', playAudioG, false);
+		// Lewis
+		var playButton = document.getElementById('bass');
+		playButton.addEventListener('click', playAudioH, false);
+		// Lewis
+		var playButton = document.getElementById('violin');
+		playButton.addEventListener('click', playAudioI, false);
 	}
 
 
 	
 	function playAudioA() {
-		newAudioA.play()
+		if (newAudioA.paused){
+				newAudioA.play()
+
+			} else {
+				newAudioA.pause()
+			}
 	}
 	function playAudioB() {
-		newAudioB.play()
+	if (newAudioB.paused){
+				newAudioB.play()
+
+			} else {
+				newAudioB.pause()
+			}
 	}
 	function playAudioC() {
-		newAudioC.play()
+	if (newAudioC.paused){
+				newAudioC.play()
+
+			} else {
+				newAudioC.pause()
+			}
 	}
 
 	function playAudioD() {
-		newAudioD.play()
-	}
+	if (newAudioD.paused){
+				newAudioD.play()
 
+			} else {
+				newAudioD.pause()
+			}
+	}
 	function playAudioE() {
-		newAudioE.play()
+	if (newAudioE.paused){
+				newAudioE.play()
+
+			} else {
+				newAudioE.pause()
+			}
+	}
+		function playAudioF() {
+	if(newAudioF.paused){
+				newAudioF.play()
+			} else {
+				newAudioF.pause()
+			}
 	}
 
+	// Lewis
+	function playAudioG() {
+		if(newAudioG.paused){
+				newAudioG.play()
+			} else {
+				newAudioG.pause()
+			}
+	}
+	// Lewis
+	function playAudioH() {
+	if(newAudioH.paused){
+				newAudioH.play()
+			} else {
+				newAudioH.pause()
+			}
+	}
+	// Lewis
+	function playAudioI() {
+	if(newAudioI.paused){
+				newAudioI.play()
+			} else {
+				newAudioI.pause()
+			}
+	}
+console.log();
+})()
